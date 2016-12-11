91739019edddef92647f5d59e451f63fddf9941f
Ertuğrul Akay <akay_ertuqrul@yahoo.com>
2016-12-11 13:58:09 +0200

    story line added

---
+++ code/Your_Story/Story_Line/Character.java
@@ -0,0 +1,58 @@
+package Story_Line;
+
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+
+/**
+ *
+ * @author kaxell
+ * Things to consider: Images. How do we really store them?  
+ */
+
+
+public class Character {
+    private String description;
+    private String name;
+    private long ID;
+    private boolean isTaken;
+   
+    BufferedImage image = null;
+    File imageFile = null;
+
+    //read image file
+    public Character(String description, String name, long ID, boolean isTaken)
+    {
+        this.description = description;
+        this.name = name;
+        this.ID = ID;
+        this.isTaken = isTaken;
+    }
+    
+    public Character()
+    {
+        this.description = "";
+        this.name = "";
+        this.ID = 0;
+        this.isTaken = false;
+    }
+    
+    //getters
+    public String getDescription(){return description;} 
+    public String getName(){return name;} 
+    public long getID(){return ID;} 
+    public boolean getIsTaken(){return isTaken;}
+    
+    //setters
+    public void setDescription(String description)
+    {this.description = description;}
+    public void setName(String name)
+    {this.name = name;}
+    public void setID(long charID)
+    {ID = charID;}
+    public void setIsTaken(boolean isTaken)
+    {this.isTaken = isTaken;}   
+    
+    
+    
+}
