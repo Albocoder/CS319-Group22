4419a8aab7aa8eafe1f1ed10b8b0e63d42bd1d23
Kaxell <ertugrul.akay.cs@gmail.com>
2016-12-18 18:15:32 +0200

    Char changed

--- code/Your_Story/ysProject/src/mainPackage/Character.java
+++ code/Your_Story/ysProject/src/mainPackage/Character.java
@@ -14,33 +14,38 @@ import java.awt.image.BufferedImage;
 public class Character {
     private String description;
     private String name;
-    private long ID;
+    private long charID;
     private boolean isTaken;
    
     BufferedImage image = null;
-    File imageFile = null;
+   
 
     //read image file
-    public Character(String description, String name, long ID, boolean isTaken)
+    public Character(String description, String name, long charID, boolean isTaken)
     {
         this.description = description;
         this.name = name;
-        this.ID = ID;
+        this.charID = charID;
         this.isTaken = isTaken;
+    }
+    
+    public Character(long charID)
+    {
+        this.charID = charID;
     }
     
     public Character()
     {
         this.description = "";
         this.name = "";
-        this.ID = 0;
+        this.charID = 0;
         this.isTaken = false;
     }
     
     //getters
     public String getDescription(){return description;} 
     public String getName(){return name;} 
-    public long getID(){return ID;} 
+    public long getCharID(){return charID;} 
     public boolean getIsTaken(){return isTaken;}
     
     //setters
@@ -48,8 +53,8 @@ public class Character {
     {this.description = description;}
     public void setName(String name)
     {this.name = name;}
-    public void setID(long charID)
-    {ID = charID;}
+    public void setCharID(long charID)
+    {this.charID = charID;}
     public void setIsTaken(boolean isTaken)
     {this.isTaken = isTaken;}   
     
