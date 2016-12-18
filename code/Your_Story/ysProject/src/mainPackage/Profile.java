064bf8743baba52c84322db3a07c1120e761f07e
Kaxell <ertugrul.akay.cs@gmail.com>
2016-12-18 12:56:34 +0200

    profile added

---
+++ code/Your_Story/ysProject/src/mainPackage/Profile.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainPackage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kaxell
+ */
+public class Profile {
+ 
+    private String name;
+    private Image photo;
+    private String description;
+    private ArrayList<Lobby> finishedGames;
+    private boolean isYours;
+    
+    public Profile( String name, Image photo, String Description, boolean isYours){
+        this.name = name;
+        this.photo = photo;
+        this.description = description;
+        this.isYours = isYours;
+        finishedGames = new ArrayList<Lobby>();
+    
+    }
+    
+    //getters / setters 
+    
+    public String getName(){return name;}
+    public Image getImage(){return photo;}
+    public String getDescription(){return description;}
+    public ArrayList<Lobby> getFinishedGames(){return finishedGames;}
+    public boolean getIsYours(){return isYours;}
+   
+    public void setName(String name){this.name = name;}
+    public void setImage(Image photo){this.image = image;}
+    public void setDescription(String description){this.description = description;}
+    public void setFinishedGames(ArrayList<Lobby> finishedGames) {this.finishedGames = finishedGames;}
+    public void setIsYours(boolean isYours){this.isYours = isYours;}
+    
+    
+    
+    
+    public void retrieveData(){
+    //Retrieves the data of profile. This will sync the variables of object
+    //with the data in the database, therefore before showing a profile to the users, this method will
+    //make it certain that the updated data will be showed to the user.
+    }
+    
+    public void updateData(){
+    //Updates the data of profile page in database with current values. This
+    //is the opposite of the retrieveData method, so this one will sync the data in the database with
+    //the current values of variables in an Profile object.
+    
+    }
+    
+    public Player createPlayer (int ID, boolean isYours){return null;}
+            /*Creates a player object with given values.
+This will be useful method when getting new player information comes from the database, by
+this method an new player instance will be created with given unique ID. The other information
+about the players will be taken with Profile class via ID variable. isYours parameter shows if new
+player object will be representative of current user of the client. If so, some actions will be
+available for the user, like updating profile data, joining a lobby.*/
+    
+    public ArrayList<Lobby> viewUnfinishedGames(){
+    //Returns unfinished games of player that profile belongs to
+    return null;
+    }
+    
+}
