985acbf12c63cf5fab9fd478d9ee388e68310e7a
Ali Gunes <ali.gunes195@gmail.com>
2016-12-24 21:14:14 +0200

    Adding InGameView some functions

--- code/Your_Story/ysProject/src/view/InGameView.java
+++ code/Your_Story/ysProject/src/view/InGameView.java
@@ -20,19 +20,22 @@ import mainPackage.*;
  */
 public class InGameView extends JFrame implements Viewable {
     
-        private ViewManager referrer;
+	private ViewManager referrer;
 	
 	static Font font;
+	static Font boldFont;
 	static int LINE_HEIGHT;
 	JScrollPane scrollPane;
 	ChatView chat;
 	JPanel onlineUsers;
+	JPanel bottomPanel;
 	MessageTypingBoxView mv;
 	Lobby l;
         
-        private ScheduledExecutorService chatExec;
-        private ScheduledExecutorService onlineUsersExec;
+	private ScheduledExecutorService chatExec;
+	private ScheduledExecutorService onlineUsersExec;
 	
+<<<<<<< 0493deb5b51a95ef3d3e819fe54a649872b5d6fe
     /**
      *
      * @param l
@@ -45,7 +48,17 @@ public class InGameView extends JFrame implements Viewable {
                 Logger.getLogger(LobbyView.class.getName()).log(Level.SEVERE, null, ex);
             }
                 logoutOnExitWithDialogue();
+=======
+	public InGameView(Lobby l, ViewManager ref) {
+		try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            Logger.getLogger(LobbyView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+>>>>>>> Adding InGameView some functions
 		font = new Font("Tahoma", Font.PLAIN, 12);
+		boldFont = new Font("Tahoma", Font.BOLD, 12);
 		LINE_HEIGHT = font.getSize() + 8;
 		referrer = ref;
 		JPanel mainPanel = new JPanel();
@@ -57,11 +70,15 @@ public class InGameView extends JFrame implements Viewable {
 		
 		JPanel panel = new JPanel();
 		onlineUsers = new JPanel();
+		JScrollPane storyScroll = new JScrollPane();
 		JPanel story = new JPanel();
+		bottomPanel = new JPanel();
 		JPanel buttonPanel = new JPanel();
 		JButton voteButton = new JButton("Vote");
 		JButton exitButton = new JButton("Exit");
 		
+		logoutOnExitWithDialogue();
+		updateView();
 		add(mainPanel);
 		addMouseListener(mv);
 		setMinimumSize(new Dimension(700, 400));
@@ -70,6 +87,7 @@ public class InGameView extends JFrame implements Viewable {
 			public void componentResized(ComponentEvent e) {
 				scrollPane.setSize(new Dimension(mv.getWidth(), 
 								mv.getHeight() - mv.getBoxHeight()));
+				storyScroll.setPreferredSize(new Dimension(300, getHeight() / 3));
 			}
 		});
 		
@@ -79,24 +97,37 @@ public class InGameView extends JFrame implements Viewable {
 		
 		panel.setPreferredSize(new Dimension(300, 400));
 		panel.setLayout(new BorderLayout());
-		panel.add(story, BorderLayout.NORTH);
+		panel.add(storyScroll, BorderLayout.NORTH);
 		panel.add(onlineUsers, BorderLayout.CENTER);
-		panel.add(buttonPanel, BorderLayout.SOUTH);
+		panel.add(bottomPanel, BorderLayout.SOUTH);
 		
-		story.add(new JLabel(new ImageIcon(createStoryImage())));
+		storyScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		storyScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		storyScroll.getVerticalScrollBar().setUnitIncrement(15);
+		storyScroll.getVerticalScrollBar().setBlockIncrement(0);
+		storyScroll.setPreferredSize(new Dimension(300, getHeight() / 3));
+		storyScroll.add(story);
+		
+		BufferedImage img = createStoryImage();
+		story.add(new JLabel(new ImageIcon(img)));
+		story.setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
 		
 		onlineUsers.add(new JLabel(new ImageIcon(createOnlineUsersImage()), SwingConstants.CENTER));
+		
+		bottomPanel.setLayout(new BorderLayout());
+		bottomPanel.add(buttonPanel, BorderLayout.SOUTH);
 		
 		buttonPanel.setLayout(new FlowLayout());
 		buttonPanel.add(voteButton);
 		buttonPanel.add(exitButton);
 		
+		voteButton.addActionListener(new VoteListener());
+		exitButton.addActionListener(new ExitListener());
+		
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.getVerticalScrollBar().setUnitIncrement(15);
 		scrollPane.getVerticalScrollBar().setBlockIncrement(0);
-		
-		updateView();
 	}
 	
     /**
@@ -243,7 +274,6 @@ public class InGameView extends JFrame implements Viewable {
     @Override
 	public void showView() {
 		setVisible(true);
-                updateView();
 	}
 	
 	private class ChatUpdater implements Runnable {
@@ -271,7 +301,7 @@ public class InGameView extends JFrame implements Viewable {
 			revalidate();
 		}
 	}
-	/*
+	
 	public static void main (String [] args) {
             java.awt.EventQueue.invokeLater(new Runnable() {
                 @Override
@@ -281,15 +311,31 @@ public class InGameView extends JFrame implements Viewable {
                     new InGameView(LobbyConnection.getLobby(9), null);
                 }
             });
-	}*/
+	}
+	
     private void logoutOnExitWithDialogue(){
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
                 terminateView();
-                //System.out.println("sdfsdfsd");
             }
         });
+    }
+    
+    private class VoteListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+//			bottomPanel.add(new );
+			mv.addNotify();
+		}
+    }
+    
+    private class ExitListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			terminateView();
+			mv.addNotify();
+		}
     }
 }
